cd6f7213648d9410bbb4ea884de56c41da164747
Nic <951477037@qq.com>
2023-04-19 11:08:55 +0800

    chore: update

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,11 +1,11 @@
 /*
  * @Author: Nic_
  * @Date: 2023-04-19 10:32:32
- * @LastEditTime: 2023-04-19 10:39:56
+ * @LastEditTime: 2023-04-19 11:08:44
  * @LastEditors: Nic_
  * @Description: 
- * @FilePath: /axios-ts-monorepo/packages/axios-core/src/index.ts
+ * @FilePath: /axios-ts-monorepo/packages/core/src/index.ts
  */
 export const test = () => {
-    console.log(`11111`, 11112)
+    console.log(`11111`, 222)
 }
