c4ad25b67f02163adb7c7ee9d0d1ab21ee9471c4
Nic <951477037@qq.com>
2023-04-19 11:33:07 +0800

    chore: add eslint

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,11 +1,11 @@
 /*
  * @Author: Nic_
  * @Date: 2023-04-19 10:32:32
- * @LastEditTime: 2023-04-19 11:08:44
+ * @LastEditTime: 2023-04-19 11:32:20
  * @LastEditors: Nic_
- * @Description: 
+ * @Description:
  * @FilePath: /axios-ts-monorepo/packages/core/src/index.ts
  */
-export const test = () => {
-    console.log(`11111`, 222)
+export function test() {
+  console.log('11111', 222)
 }
