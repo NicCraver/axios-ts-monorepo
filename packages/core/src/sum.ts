2bdff6538e693eb88d1f5c91ef3934f63c13d677
Nic <951477037@qq.com>
2023-04-20 10:35:47 +0800

    chore: update

--- packages/core/src/sum.ts
+++ packages/core/src/sum.ts
@@ -1,11 +1,18 @@
 /*
  * @Author: Nic_
  * @Date: 2023-04-19 13:25:33
- * @LastEditTime: 2023-04-19 13:25:38
+ * @LastEditTime: 2023-04-20 10:02:42
  * @LastEditors: Nic_
  * @Description:
  * @FilePath: /axios-ts-monorepo/packages/core/src/sum.ts
  */
-export default function sum(...numbers: number[]) {
-  return numbers.reduce((total, number) => total + number, 0)
+import type { AxiosInstance } from 'axios'
+
+interface a {
+  a: AxiosInstance
+}
+// console.log('AxiosInstance', AxiosInstance)
+export default function sum(axios: a) {
+  console.log('axios', axios)
+  // return numbers.reduce((total, number) => total + number, 0)
 }
